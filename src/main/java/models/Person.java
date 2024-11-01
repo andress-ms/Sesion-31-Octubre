148e6d97487cc57a0348d996fe58b66a634eb530
jadg13 <86695996+jadg-13@users.noreply.github.com>
2024-10-31 18:06:39 -0600

    added classes Person and Product

---
+++ src/main/java/models/Person.java
@@ -0,0 +1,55 @@
+package models;
+
+public class Person {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void printInformation() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Gender: " + gender);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
